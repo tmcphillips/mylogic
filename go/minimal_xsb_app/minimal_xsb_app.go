4facad6ceaac09359138235be4660993f97c7765
Timothy McPhillips <tmcphillips@absoluteflow.com>
2019-07-23 14:23:39 -0700

    minimal Go app that includes XSB API header file

---
+++ go/minimal_xsb_app/minimal_xsb_app.go
@@ -0,0 +1,12 @@
+package main
+
+/*
+#cgo CFLAGS: -I/home/tmcphill/XSB/emu/
+#cgo CFLAGS: -I/home/tmcphill/XSB/config/x86_64-unknown-linux-gnu/
+
+#include "cinterf.h"
+*/
+import "C"
+
+func main() {
+}
