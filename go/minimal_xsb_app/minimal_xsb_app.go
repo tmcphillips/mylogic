de49177c57530ce72e7ef3938d2dad466503373e
Timothy McPhillips <tmcphillips@absoluteflow.com>
2019-07-23 18:40:35 -0700

    Go app now links to XSB and initializes it at runtime

--- go/minimal_xsb_app/minimal_xsb_app.go
+++ go/minimal_xsb_app/minimal_xsb_app.go
@@ -4,9 +4,22 @@ package main
 #cgo CFLAGS: -I/home/tmcphill/XSB/emu/
 #cgo CFLAGS: -I/home/tmcphill/XSB/config/x86_64-unknown-linux-gnu/
 
+#include <stdio.h>
+#include <string.h>
 #include "cinterf.h"
+
+void initialize_xsb() {
+	char init_string[1024];
+	strcpy(init_string, "/home/tmcphill/XSB/");
+	if (xsb_init_string(init_string) == XSB_ERROR) {
+		fprintf(stderr, "Error initializing XSB: %s/%s\n",
+			xsb_get_init_error_type(), xsb_get_init_error_message());
+		exit(XSB_ERROR);
+	}
+}
 */
 import "C"
 
 func main() {
+	C.initialize_xsb()
 }
